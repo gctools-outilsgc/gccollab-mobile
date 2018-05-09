757ba161e90eba29749e4540e7ce07c7c3693ff0
Steph4104 <stephanie4104@gmail.com>
2018-05-09 07:18:55 -0400

    start adding tabs

--- www/js/GCT.js
+++ www/js/GCT.js
@@ -1308,6 +1308,48 @@ GCTUser = {
             }
         });
     },
+
+    GetEventsByUser: function (from, to, limit, successCallback, errorCallback) {
+        limit = limit || 10;
+       // offset = offset || 0;
+        from = from || "";
+        to = to || "";
+
+        $$.ajax({
+            method: 'POST',
+            dataType: 'json',
+            url: GCT.GCcollabURL,
+            data: { method: "get.eventsbyowner", user: GCTUser.Email(), from: from, to: to, limit: limit, api_key: api_key_gccollab, lang: GCTLang.Lang() },
+            timeout: 12000,
+            success: function (data) {
+                successCallback(data);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                errorCallback(jqXHR, textStatus, errorThrown);
+            }
+        });
+    },
+
+    GetEventsByColleagues: function (from, to, limit, offset, successCallback, errorCallback) {
+        limit = limit || 10;
+        from = from || "";
+        to = to || "";
+
+        $$.ajax({
+            method: 'POST',
+            dataType: 'json',
+            url: GCT.GCcollabURL,
+            data: { method: "get.eventsbycolleagues", user: GCTUser.Email(), from: from, to: to, limit: limit, api_key: api_key_gccollab, lang: GCTLang.Lang() },
+            timeout: 12000,
+            success: function (data) {
+                successCallback(data);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(jqXHR);
+                errorCallback(jqXHR, textStatus, errorThrown);
+            }
+        });
+    },
     GetGroup: function (guid, successCallback, errorCallback) {
         $$.ajax({
             method: 'POST',
